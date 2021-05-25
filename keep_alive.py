cd4632eb219defc70d03225b9eca5db6319f5757
Gyeong Dok Ko <42568666+lemidia@users.noreply.github.com>
2021-05-25 15:14:27 +0900

    some comment added

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,16 @@
+# This code make a web server on replit.
+
+# For receiving a ping from monitoring server that might be operating somewhere.
+
+# The reason why we make a web server and receve a ping is that
+# replit server will go sleep mode if there is no actual situation(typing, ping, etc) on the web server.
+
+# We use this code to keep alive our discord bot.
+
+# Monitoring server that send a ping to a web server refer to following URL.
+
+# https://uptimerobot.com/
+
 from flask import Flask
 from threading import Thread
 
